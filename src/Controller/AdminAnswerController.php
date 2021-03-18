f7e6e5517e6bb8a7778bbe94858b49738cd63cef
valharault <valentinharault1@gmail.com>
2021-03-18 20:35:20 +0100

    front_chatbot

--- src/Controller/AdminAnswerController.php
+++ src/Controller/AdminAnswerController.php
@@ -3,20 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Answer;
-use App\Entity\AnswerLink;
-use App\Entity\User;
-use App\Form\AdminUserEditType;
-use App\Form\AdminUserNewType;
-use App\Repository\AnswerLinkRepository;
 use App\Repository\AnswerRepository;
-use App\Repository\UserRepository;
-use Psr\Log\LoggerInterface;
+use App\Service\ChatbotService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * @Route("/admin/chatbot")
@@ -27,7 +20,7 @@ class AdminAnswerController extends AbstractController
     /**
      * @Route("/", name="chatbot_view", methods={"GET"})
      */
-    public function index(AnswerRepository $answerRepository): Response
+    public function index(AnswerRepository $answerRepository, ChatbotService $chatbotService): Response
     {
         $firstAnswers = $answerRepository->findBy(['first' => true]);
         $array = [];
@@ -36,68 +29,27 @@ class AdminAnswerController extends AbstractController
                 'id' => $firstAnswer->getId(),
                 'text' => $firstAnswer->getText()
             ];
-            $array = $this->recursiveSerialize($firstAnswer, $array);
+            $array = $chatbotService->recursiveSerialize($firstAnswer, $array);
         }
         return $this->render('admin/answer/index.html.twig', [
             'array' => json_encode($array)
         ]);
     }
 
-    protected function recursiveSerialize(Answer $answer, $array = null)
-    {
-        foreach ($answer->getTargetAnswer() as $targetAnswer) {
-            foreach ($array as $index => $item) {
-                if ($item['id'] === $targetAnswer->getSourceAnswer()->getId()) {
-                    $array[$index]['children'][] = [
-                        'id' => $targetAnswer->getTargetAnswer()->getId(),
-                        'text' => $targetAnswer->getTargetAnswer()->getText()
-                    ];
-                    if (!$targetAnswer->getTargetAnswer()->getTargetAnswer()->isEmpty()) {
-                        $array[$index]['children'] = $this->recursiveSerialize($targetAnswer->getTargetAnswer(), $array[$index]['children']);
-                    }
-
-                }
-            }
-        }
-        return $array;
-    }
-
-    protected function recursiveUnserialize(Answer $answer, $data, $arrayAnswers = null)
-    {
-        if (isset($data['children']) && !empty($data['children'])) {
-            foreach ($data['children'] as $child) {
-
-                $childAnswer = (new Answer())
-                    ->setText($child['text'])
-                    ->setFirst(false)
-                    ->setCreatedAt(new \DateTime());
-
-                $answerLink = (new AnswerLink())
-                    ->setSourceAnswer($answer)
-                    ->setTargetAnswer($childAnswer);
-
-                $answer->addAnswerLinks($answerLink);
-                $this->recursiveUnserialize($childAnswer, $child);
-            }
-        }
-        return $answer;
-    }
-
-
     /**
      * @Route("/update", name="chatbot_update")
      */
-    public function changeState(Request $request, AnswerRepository $answerRepository, LoggerInterface $logger)
+    public function changeState(Request $request, AnswerRepository $answerRepository, ChatbotService $chatbotService)
     {
         if ($request->isXmlHttpRequest()) {
             if (!empty($datas = ($request->request->get('data')))) {
-                $this->cleanAnswer($answerRepository);
+                $chatbotService->cleanAnswer($answerRepository);
                 foreach ($datas as $data) {
                     $answer = (new Answer())
                         ->setCreatedAt(new \DateTime())
                         ->setFirst(true)
                         ->setText($data['text']);
-                    $answer = $this->recursiveUnserialize($answer, $data);
+                    $answer = $chatbotService->recursiveUnserialize($answer, $data);
                     $this->getDoctrine()->getManager()->persist($answer);
                     $this->getDoctrine()->getManager()->flush();
                 }
@@ -105,13 +57,5 @@ class AdminAnswerController extends AbstractController
             }
         }
         return $this->json(['type' => 'error', 'message' => 'Erreur'], 400);
-    }
-
-    protected function cleanAnswer(AnswerRepository $answerRepository)
-    {
-        foreach ($answerRepository->findAll() as $answer) {
-            $this->getDoctrine()->getManager()->remove($answer);
-        }
-        $this->getDoctrine()->getManager()->flush();
     }
 }
